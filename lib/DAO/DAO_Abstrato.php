693951b0f3141f619d4f4e89f6ce667332bc8063
macaroots <58048446+macaroots@users.noreply.github.com>
2020-08-17 15:13:51 -0300

    Exibindo erros do PDO

--- lib/DAO/DAO_Abstrato.php
+++ lib/DAO/DAO_Abstrato.php
@@ -2,6 +2,7 @@
 class DAO_Abstrato {
 	function conecta() {
 		$conexao = new PDO('mysql:host=localhost;dbname=empresa', 'root', 'admin');
+		$conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		return $conexao;
 	}
 }
